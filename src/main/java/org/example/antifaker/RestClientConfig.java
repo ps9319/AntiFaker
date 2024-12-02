509c8076025a980f6b63e0199269d7480675f517
박민규 <pmq0101@naver.com>
2024-12-02 16:48:51 +0900

    refactor(RestClientConfig): restClientconfig 수정

--- src/main/java/org/example/antifaker/RestClientConfig.java
+++ src/main/java/org/example/antifaker/RestClientConfig.java
@@ -2,24 +2,16 @@ package org.example.antifaker;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.http.converter.FormHttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class RestClientConfig {
 
     @Bean
-    public RestClient restClient(RestClient.Builder builder) {
-        return builder
+    public RestClient restClient() {
+        return RestClient.builder()
             .baseUrl("http://localhost:8000")
             .defaultHeader("Content-Type", "multipart/form-data")
-            .requestFactory(new HttpComponentsClientHttpRequestFactory())
-            .messageConverters(converters -> {
-                converters.add(new FormHttpMessageConverter());
-                converters.add(new MappingJackson2HttpMessageConverter());
-            })
             .build();
     }
 }
