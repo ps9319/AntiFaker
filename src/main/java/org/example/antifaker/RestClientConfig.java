cd9b33d43d373f99325ffc2be73eed9694080e46
박민규 <pmq0101@naver.com>
2024-12-02 16:38:17 +0900

    feat(RestClientConfig): RestClient 설정 추가

--- src/main/java/org/example/antifaker/RestClientConfig.java
+++ src/main/java/org/example/antifaker/RestClientConfig.java
@@ -2,16 +2,24 @@ package org.example.antifaker;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.converter.FormHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class RestClientConfig {
 
     @Bean
-    public RestClient restClient() {
-        return RestClient.builder()
-            // 파이썬 서버 주소
-            .baseUrl("http://localhost:8080")
+    public RestClient restClient(RestClient.Builder builder) {
+        return builder
+            .baseUrl("http://localhost:8000")
+            .defaultHeader("Content-Type", "multipart/form-data")
+            .requestFactory(new HttpComponentsClientHttpRequestFactory())
+            .messageConverters(converters -> {
+                converters.add(new FormHttpMessageConverter());
+                converters.add(new MappingJackson2HttpMessageConverter());
+            })
             .build();
     }
 }
